d2aacfca89883e05706fa0e07a10e58b1d8296ea
Nikolay <razenkov.rna@mail.ru>
2023-05-26 05:27:20 -0700

    initial Commit

---
+++ src/main/java/com/example/demoanimalbot/DemoAnimalBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoanimalbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAnimalBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAnimalBotApplication.class, args);
+    }
+
+}
