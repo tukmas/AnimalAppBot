3842a4f8f8031b88f260de970c357379be082b9a
Nikolay <razenkov.rna@mail.ru>
2023-06-05 10:44:43 -0700

    Тесты

---
+++ src/test/java/com/example/demoanimalbot/service/PetServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.demoanimalbot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetServiceTest {
+
+    @Test
+    void createCat() {
+    }
+
+    @Test
+    void createDog() {
+    }
+
+    @Test
+    void findCat() {
+    }
+
+    @Test
+    void findDog() {
+    }
+
+    @Test
+    void takeCatAtHome() {
+    }
+
+    @Test
+    void takeDogAtHome() {
+    }
+
+    @Test
+    void findCatsByUserId() {
+    }
+
+    @Test
+    void findDogsByUserId() {
+    }
+}
